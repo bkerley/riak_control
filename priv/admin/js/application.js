531e90c85567a60902f1598051220c91b7c10a02
Bryce Kerley <bkerley@brycekerley.net>
2012-02-28 11:11:02 -0500

    try ico instead of g.raph

--- priv/admin/js/application.js
+++ priv/admin/js/application.js
@@ -1,45 +1,138 @@
 (function() {
 
   $(document).ready(function() {
-    var ErlangMemoryChart, rand;
-    rand = Math.random;
-    ErlangMemoryChart = (function() {
+    var poll, rand,
+      _this = this;
+    window.Start = function() {
+      window.Cluster = new window.RiakCluster();
+      window.Chart = new window.MemoryChartView({
+        model: window.Cluster
+      });
+      return poll();
+    };
+    poll = function() {
+      window.Cluster.load();
+      return window.setTimeout(poll, 500);
+    };
+    window.RiakCluster = (function() {
 
-      function ErlangMemoryChart() {
-        this.scheduleNext();
-        this.canvases = [$('#canvas-1'), $('#canvas-2')];
-        this.bufs = [Raphael('canvas-1', 640, 480), Raphael('canvas-2', 640, 480)];
+      function RiakCluster() {
+        this.nodes = [];
       }
 
-      ErlangMemoryChart.prototype.scheduleNext = function() {
-        var hello,
-          _this = this;
-        hello = function() {
-          return _this.drawGraph.apply(_this);
-        };
-        return window.setTimeout(hello, 1000);
-      };
-
-      ErlangMemoryChart.prototype.drawGraph = function() {
+      RiakCluster.prototype.load = function() {
         var _this = this;
         return $.getJSON('/admin/cluster/list', function(j) {
-          _this.backbuf = _this.bufs[1];
-          _this.chart = _this.backbuf.linechart(0, 0, 640, 480, [1, 2, 3, 4, 5], [[rand(), rand(), rand(), rand(), rand()], [rand(), rand(), rand(), rand(), rand()]], {
-            shade: true
-          });
-          _this.canvases[1].removeClass('hide');
-          _this.canvases[0].addClass('hide');
-          _this.canvases.reverse();
-          _this.bufs.reverse();
-          _this.bufs[1].clear();
-          return _this.scheduleNext();
+          return _this.consume(j);
         });
+      };
+
+      RiakCluster.prototype.consume = function(returned) {
+        var entry;
+        this.nodes = (function() {
+          var _i, _len, _results;
+          _results = [];
+          for (_i = 0, _len = returned.length; _i < _len; _i++) {
+            entry = returned[_i];
+            _results.push(new RiakNode(entry));
+          }
+          return _results;
+        })();
+        return window.Chart.trigger('cluster:loaded');
+      };
+
+      return RiakCluster;
+
+    })();
+    window.RiakNode = (function() {
+
+      function RiakNode(d) {
+        this.d = d;
+        this.mem_total_history = [];
+        this.mem_used_history = [];
+        this.mem_erlang_history = [];
+        this.update(this.d);
+      }
+
+      RiakNode.prototype.update = function(d) {
+        this.d = d;
+        _.extend(this, this.d);
+        return this.shiftHistory();
+      };
+
+      RiakNode.prototype.shiftHistory = function() {
+        this.mem_total_history.push(this.mem_total / (1024 * 1024));
+        if (this.mem_total_history.length > 20) this.mem_total_history.shift();
+        this.mem_used_history.push(this.mem_used / (1024 * 1024));
+        if (this.mem_used_history.length > 20) this.mem_used_history.shift();
+        this.mem_erlang_history.push(this.mem_erlang / (1024 * 1024));
+        if (this.mem_erlang_history.length > 20) {
+          return this.mem_erlang_history.shift();
+        }
+      };
+
+      return RiakNode;
+
+    })();
+    window.ErlangMemoryChart = (function() {
+
+      function ErlangMemoryChart() {
+        this.canvases = [$('#canvas-1'), $('#canvas-2')];
+        this.bufs = ['canvas-1', 'canvas-2'];
+      }
+
+      ErlangMemoryChart.prototype.drawGraph = function(cluster) {
+        var chart_data, chart_range, history_size, node;
+        history_size = cluster.nodes[0].mem_erlang_history.length;
+        chart_data = (function() {
+          var _i, _len, _ref, _results;
+          _ref = cluster.nodes;
+          _results = [];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            node = _ref[_i];
+            _results.push(node.mem_erlang_history);
+          }
+          return _results;
+        })();
+        chart_range = (function() {
+          var _i, _len, _ref, _results;
+          _ref = cluster.nodes;
+          _results = [];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            node = _ref[_i];
+            _results.push(_.range(0, history_size));
+          }
+          return _results;
+        })();
+        this.backbuf = this.bufs[1];
+        this.chart = new Ico.LineGraph(this.backbuf, chart_data, {
+          width: 640,
+          height: 480
+        });
+        this.canvases[1].removeClass('hide');
+        this.canvases[0].addClass('hide');
+        this.canvases.reverse();
+        this.bufs.reverse();
+        return this.canvases[1].html('');
       };
 
       return ErlangMemoryChart;
 
     })();
-    return window.hi = new ErlangMemoryChart;
+    window.MemoryChartView = Backbone.View.extend({
+      initialize: function() {
+        var _this = this;
+        this.chart = new ErlangMemoryChart;
+        return this.on('cluster:loaded', function() {
+          return _this.render();
+        });
+      },
+      tagName: 'div',
+      render: function() {
+        return this.chart.drawGraph(this.model);
+      }
+    });
+    return rand = Math.random;
   });
 
 }).call(this);
--- priv/source/js/application.coffee
+++ priv/source/js/application.coffee
@@ -1,28 +1,67 @@
 $(document).ready ->
-  rand = Math.random
-  class ErlangMemoryChart
+  window.Start = ->
+    window.Cluster = new window.RiakCluster()
+    window.Chart = new window.MemoryChartView({model: window.Cluster})
+    poll()
+  poll = =>
+    window.Cluster.load()
+    window.setTimeout(poll, 500)
+
+  class window.RiakCluster
     constructor: ->
-      @scheduleNext()
+      @nodes = []
+    load: ->
+      $.getJSON '/admin/cluster/list', (j) =>
+        @consume j
+    consume: (returned) ->
+      @nodes = (new RiakNode(entry) for entry in returned)
+      window.Chart.trigger('cluster:loaded')
+
+  class window.RiakNode
+    constructor: (@d) ->
+      @mem_total_history = []
+      @mem_used_history = []
+      @mem_erlang_history = []
+      @update(@d)
+    update: (@d) ->
+      _.extend(this, @d)
+      @shiftHistory()
+    shiftHistory: ->
+      @mem_total_history.push @mem_total / (1024*1024)
+      @mem_total_history.shift() if @mem_total_history.length > 20
+      @mem_used_history.push @mem_used / (1024*1024)
+      @mem_used_history.shift() if @mem_used_history.length > 20
+      @mem_erlang_history.push @mem_erlang / (1024*1024)
+      @mem_erlang_history.shift() if @mem_erlang_history.length > 20
+
+  class window.ErlangMemoryChart
+    constructor: ->
       @canvases = [$('#canvas-1'), $('#canvas-2')]
-      @bufs = [Raphael('canvas-1', 640, 480), Raphael('canvas-2', 640, 480)]
-    scheduleNext: ->
-      hello = => @drawGraph.apply(this)
-      window.setTimeout(hello, 1000)
-    drawGraph: ->
-      $.getJSON('/admin/cluster/list', (j) =>
-        @backbuf = @bufs[1]
-        @chart = @backbuf.linechart(0, 0, 640, 480, [1, 2, 3, 4, 5],
-        [
-          [rand(), rand(), rand(), rand(), rand()],
-          [rand(), rand(), rand(), rand(), rand()]],
-          {
-            shade: true
-          })
-        @canvases[1].removeClass('hide')
-        @canvases[0].addClass('hide')
-        @canvases.reverse()
-        @bufs.reverse()
-        @bufs[1].clear()
-        @scheduleNext()
-      )
-  window.hi = new ErlangMemoryChart
+      @bufs = ['canvas-1', 'canvas-2']
+    drawGraph: (cluster)->
+      history_size = cluster.nodes[0].mem_erlang_history.length
+      chart_data = (node.mem_erlang_history for node in cluster.nodes)
+      chart_range = (_.range(0, history_size) for node in cluster.nodes)
+      @backbuf = @bufs[1]
+      @chart = new Ico.LineGraph @backbuf, chart_data, {
+        width: 640,
+        height: 480,
+        meanline: true,
+        units: 'MB'
+        }
+      @canvases[1].removeClass('hide')
+      @canvases[0].addClass('hide')
+      @canvases.reverse()
+      @bufs.reverse()
+      @canvases[1].html('')
+
+  window.MemoryChartView = Backbone.View.extend
+    initialize: ->
+      @chart = new ErlangMemoryChart
+      @on 'cluster:loaded', => @render()
+    tagName: 'div'
+    render: ->
+      @chart.drawGraph(@model)
+
+  rand = Math.random
+  # start()
