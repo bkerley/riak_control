d824acc21c777f9b73f38873f32259570d021008
Bryce Kerley <bkerley@brycekerley.net>
2012-02-28 05:56:02 -0500

    every day i'm double-bufferin'

--- priv/admin/js/application.js
+++ priv/admin/js/application.js
@@ -1,31 +1,45 @@
 (function() {
 
   $(document).ready(function() {
-    var Hello, hi;
-    Hello = (function() {
+    var ErlangMemoryChart, rand;
+    rand = Math.random;
+    ErlangMemoryChart = (function() {
 
-      function Hello() {
+      function ErlangMemoryChart() {
         this.scheduleNext();
+        this.canvases = [$('#canvas-1'), $('#canvas-2')];
+        this.bufs = [Raphael('canvas-1', 640, 480), Raphael('canvas-2', 640, 480)];
       }
 
-      Hello.prototype.scheduleNext = function() {
+      ErlangMemoryChart.prototype.scheduleNext = function() {
         var hello,
           _this = this;
         hello = function() {
-          return _this.writeHello.apply(_this);
+          return _this.drawGraph.apply(_this);
         };
         return window.setTimeout(hello, 1000);
       };
 
-      Hello.prototype.writeHello = function() {
-        $('#toots').append("<li>asdf</li>");
-        return this.scheduleNext();
+      ErlangMemoryChart.prototype.drawGraph = function() {
+        var _this = this;
+        return $.getJSON('/admin/cluster/list', function(j) {
+          _this.backbuf = _this.bufs[1];
+          _this.chart = _this.backbuf.linechart(0, 0, 640, 480, [1, 2, 3, 4, 5], [[rand(), rand(), rand(), rand(), rand()], [rand(), rand(), rand(), rand(), rand()]], {
+            shade: true
+          });
+          _this.canvases[1].removeClass('hide');
+          _this.canvases[0].addClass('hide');
+          _this.canvases.reverse();
+          _this.bufs.reverse();
+          _this.bufs[1].clear();
+          return _this.scheduleNext();
+        });
       };
 
-      return Hello;
+      return ErlangMemoryChart;
 
     })();
-    return hi = new Hello;
+    return window.hi = new ErlangMemoryChart;
   });
 
 }).call(this);
--- priv/source/js/application.coffee
+++ priv/source/js/application.coffee
@@ -1,12 +1,28 @@
 $(document).ready ->
-  class Hello
+  rand = Math.random
+  class ErlangMemoryChart
     constructor: ->
       @scheduleNext()
+      @canvases = [$('#canvas-1'), $('#canvas-2')]
+      @bufs = [Raphael('canvas-1', 640, 480), Raphael('canvas-2', 640, 480)]
     scheduleNext: ->
-      hello = => @writeHello.apply(this)
+      hello = => @drawGraph.apply(this)
       window.setTimeout(hello, 1000)
-    writeHello: ->
-      $('#toots').append("<li>asdf</li>")
-      @scheduleNext()
-
-  hi = new Hello
+    drawGraph: ->
+      $.getJSON('/admin/cluster/list', (j) =>
+        @backbuf = @bufs[1]
+        @chart = @backbuf.linechart(0, 0, 640, 480, [1, 2, 3, 4, 5],
+        [
+          [rand(), rand(), rand(), rand(), rand()],
+          [rand(), rand(), rand(), rand(), rand()]],
+          {
+            shade: true
+          })
+        @canvases[1].removeClass('hide')
+        @canvases[0].addClass('hide')
+        @canvases.reverse()
+        @bufs.reverse()
+        @bufs[1].clear()
+        @scheduleNext()
+      )
+  window.hi = new ErlangMemoryChart
